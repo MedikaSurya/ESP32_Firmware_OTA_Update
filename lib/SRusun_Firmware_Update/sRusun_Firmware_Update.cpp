8f37f118e23a26f71ce6f499fe9a30a7539310ff
Medika Surya <im.surya98@gmail.com>
2026-04-11 15:10:47 +0700

    firmware_1_bin

---
+++ lib/SRusun_Firmware_Update/sRusun_Firmware_Update.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
+#include <WiFiClientSecure.h>
+
+#include "sRusun_Firmware_Update.h"
+
+const char* firmware_url = "https://github.com/MedikaSurya/ESP32_Firmware_OTA_Update/raw/main/.pio/build/denky32/firmware.bin";
+
+void performOTA() 
+{
+  Serial.println("Starting OTA Update from GitHub...");
+  
+  WiFiClientSecure client;
+  // Setting insecure skips SSL certificate validation. 
+  // For production, you should provide the GitHub Root CA certificate instead.
+  client.setInsecure(); 
+
+  HTTPClient http;
+  http.begin(client, firmware_url);
+
+  int httpCode = http.GET();
+  if (httpCode == HTTP_CODE_OK) 
+  {
+    int contentLength = http.getSize();
+    Serial.printf("Firmware size: %d bytes\n", contentLength);
+
+    // Check if there is enough space to begin OTA
+    bool canBegin = Update.begin(contentLength);
+    if (canBegin) 
+    {
+      Serial.println("Writing firmware to ESP32...");
+      size_t written = Update.writeStream(http.getStream());
+
+      if (written == contentLength) 
+      {
+        Serial.println("Written successfully!");
+        if (Update.end()) 
+        {
+          Serial.println("OTA Complete. Restarting...");
+          ESP.restart(); // Restart into the new firmware
+
+
+        } else 
+        {
+          Serial.printf("OTA End Failed: %s\n", Update.errorString());
+        }
+
+      }
+
+      else 
+      {
+        Serial.println("Written bytes do not match content length.");
+      }
+    }
+
+    else 
+    {
+      Serial.println("Not enough space to begin OTA.");
+    }
+  }
+   
+  else 
+  {
+    Serial.printf("HTTP GET failed, error code: %d\n", httpCode);
+  }
+  http.end();
+}
---
+++ lib/SRusun_Firmware_Update/sRusun_Firmware_Update.h
@@ -0,0 +1,13 @@
+#ifndef SRUSUN_FIRMWARE_UPDATE_H
+#define SRUSUN_FIRMWARE_UPDATE_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
+#include <WiFiClientSecure.h>
+
+
+void performOTA();
+
+#endif
