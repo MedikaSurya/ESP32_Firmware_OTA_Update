7059da9e95bc271a031b34a053b070f811402ac9
Medika Surya <im.surya98@gmail.com>
2026-04-12 16:00:46 +0700

    12/04/2026

--- .pio/build/denky32/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/denky32/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/Support/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/Support/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
